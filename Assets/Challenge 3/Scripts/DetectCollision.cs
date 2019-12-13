eab2e190adc212b118142877364452dc418ff2fd
rick325601 <56115492+rick325601@users.noreply.github.com>
2019-12-13 14:19:44 +0100

    no message

---
+++ Assets/Challenge 3/Scripts/DetectCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+    }
+}
